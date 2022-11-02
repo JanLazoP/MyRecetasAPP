53e381f0fd9dcaa35cbf2cec5379598a3b02825e
jann_ <lazopizarro2121@gmail.com>
2022-11-02 00:31:54 -0300

    iniciar

---
+++ app/src/main/java/com/novita/myrecetasapp/ui/gallery/TodoRecetasFragment.java
@@ -0,0 +1,24 @@
+package com.novita.myrecetasapp.ui.gallery;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.novita.myrecetasapp.R;
+
+public class TodoRecetasFragment extends Fragment {
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.fragment_todo_recetas,container,false);
+
+        return root;
+    }
+
+}
---
+++ app/src/main/java/com/novita/myrecetasapp/ui/slideshow/FavoritosFragment.java
@@ -0,0 +1,23 @@
+package com.novita.myrecetasapp.ui.slideshow;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.novita.myrecetasapp.R;
+
+public class FavoritosFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.fragment_favoritos,container,false);
+
+        return root;
+
+    }
+}
