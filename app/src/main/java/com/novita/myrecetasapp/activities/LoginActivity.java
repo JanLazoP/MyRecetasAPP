6da234428e164d453908c9cf52d33df5dfdf19b7
DANIEL <dvespac@gmail.com>
2022-11-27 00:59:18 +0100

    login y registro con base datos

--- app/src/main/java/com/novita/myrecetasapp/activities/LoginActivity.java
+++ app/src/main/java/com/novita/myrecetasapp/activities/LoginActivity.java
@@ -1,28 +1,86 @@
 package com.novita.myrecetasapp.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.novita.myrecetasapp.MainActivity;
 import com.novita.myrecetasapp.R;
 
 public class LoginActivity extends AppCompatActivity {
+    Button button_ingresar;
+    EditText editTextTextEmail, editTextTextPassword;
+    FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        editTextTextEmail = findViewById(R.id.editTextTextEmail);
+        editTextTextPassword = findViewById(R.id.editTextTextPassword);
+        button_ingresar = findViewById(R.id.button_ingresar);
+        mAuth = FirebaseAuth.getInstance();
+
+        button_ingresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String emailUSU = editTextTextEmail.getText().toString().trim();
+                String passUsu = editTextTextPassword.getText().toString().trim();
+
+                if (emailUSU.isEmpty() && passUsu.isEmpty()){
+                    Toast.makeText(LoginActivity.this, "Llene Todos los Campos", Toast.LENGTH_SHORT).show();
+                }else{
+                    loginUser(emailUSU,passUsu);
+
+                }
+            }
+        });{
+
+        }
     }
+
+    private void loginUser(String emailUSU, String passUsu) {
+        mAuth.signInWithEmailAndPassword(emailUSU, passUsu).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    finish();
+                    startActivity(new Intent(LoginActivity.this,MainActivity.class));
+                    Toast.makeText(LoginActivity.this, "Bienvenido,Disfrute De las recetas", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(LoginActivity.this, "Contraseña o Usuario Incorrecto", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(LoginActivity.this, "Error al Iniciar Sesion", Toast.LENGTH_SHORT).show();
+            }
+        });
+        }
+    }
+
 
     //redireccionamiento de botones
-    public void registrar(View view) {
-        startActivity(new Intent(LoginActivity.this, Registrarse.class));
-    }
+    //public void registrar(View view) {
+      //  startActivity(new Intent(LoginActivity.this, Registrarse.class));
+    //}
 
-    public void MainActividad(View view) {
-        startActivity(new Intent(LoginActivity.this, MainActivity.class));
-    }
-}
+    //public void MainActividad(View view) {
+      //  startActivity(new Intent(LoginActivity.this, MainActivity.class));
+    //}
+//}
--- app/src/main/java/com/novita/myrecetasapp/activities/Registrarse.java
+++ app/src/main/java/com/novita/myrecetasapp/activities/Registrarse.java
@@ -1,27 +1,97 @@
 package com.novita.myrecetasapp.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import java.util.HashMap;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.novita.myrecetasapp.R;
+import java.util.Map;
 
 public class Registrarse extends AppCompatActivity {
+
+    Button registrarsebtn;
+    EditText nombre,correo,contrasena;
+    FirebaseFirestore mFS;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrarse);
-    }
 
-    //redireccionamiento de botones
-    public void login(View view) {
-        startActivity(new Intent(Registrarse.this, LoginActivity.class));
-    }
+        nombre = findViewById(R.id.nombre);
+        correo = findViewById(R.id.correo);
+        contrasena = findViewById(R.id.contrasena);
+        registrarsebtn = findViewById(R.id.registrarsebtn);
+        mFS = FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
 
-    public void registrar(View view) {
-        startActivity(new Intent(Registrarse.this,LoginActivity.class));
-    }
+        registrarsebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nomUsu = nombre.getText().toString().trim();
+                String emailUsu = correo.getText().toString().trim();
+                String passUsu = contrasena.getText().toString().trim();
+
+
+                if (nomUsu.isEmpty() && emailUsu.isEmpty() && passUsu.isEmpty()){
+                    Toast.makeText(Registrarse.this, "LLene Todos Los Campos", Toast.LENGTH_SHORT).show();
+                }else {RegisterUser(nomUsu,emailUsu,passUsu);
+            }
+        }
+        });
 }
+    private void RegisterUser(String nomUsu, String emailUsu, String passUsu) {
+   // if (contrasena.getText().equals(repetircontrasena.getText())){
+
+     // }else{
+       //     Toast.makeText(this, "Las Contraseñas no coinciden", Toast.LENGTH_SHORT).show();
+        //}
+        mAuth.createUserWithEmailAndPassword(emailUsu,passUsu).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                String id = mAuth.getCurrentUser().getUid();
+                Map<String,Object> map = new HashMap<>();
+                map.put("id",id);
+                map.put("nombre",nomUsu);
+                map.put("correo",emailUsu);
+                map.put("contraseña",passUsu);
+
+
+                mFS.collection("user").document(id).set(map).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        finish();
+                        startActivity(new Intent(Registrarse.this,LoginActivity.class));
+
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                        Toast.makeText(Registrarse.this, "Error Al Guardar", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(Registrarse.this, e.toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    }
